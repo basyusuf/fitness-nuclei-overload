e7e1572b90c6392eb358a9d9146e0c76b74658b5
basyusuf <y.penava@hotmail.com>
2020-04-23 20:38:12 +0300

    Font awesome adding

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
-
+import './main.css';
+import "font-awesome/css/font-awesome.css";
 class Main extends Component {
     render() {
         return (
-            <div>
-                Welcome My Side Project
+            <div className="main">
+                <div>
+                    test
+                    <i className="fa fa-hourglass-half"></i>
+                </div>
             </div>
         );
     }
